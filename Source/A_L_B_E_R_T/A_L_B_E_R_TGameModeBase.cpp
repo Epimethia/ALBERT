c47defa7d66ecbef10078a2b36a49283daeca1cc
Epimethia <jaden.links@gmail.com>
2018-07-20 11:14:35 +1200

    Fixed some stuff

--- Source/A_L_B_E_R_T/A_L_B_E_R_TGameModeBase.cpp
+++ Source/A_L_B_E_R_T/A_L_B_E_R_TGameModeBase.cpp
@@ -14,26 +14,26 @@ void AA_L_B_E_R_TGameModeBase::StartPlay() {
 AA_L_B_E_R_TGameModeBase::AA_L_B_E_R_TGameModeBase(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
 {
-	if (GetWorld() == nullptr) {
-		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Yellow, TEXT("World pointer is null"));
-		return;
-	}
-	
-	//The player pawn class
+	//if (GetWorld() == nullptr) {
+	//	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Yellow, TEXT("World pointer is null"));
+	//	return;
+	//}
+	//
+	////The player pawn class
 	DefaultPawnClass = APlayerShip::StaticClass();
 
-	//Generating ships
-	for (int i = 0; i < 10; ++i) {
-		AViking_Ram_Ship* Ship = GetWorld()->SpawnActor<AViking_Ram_Ship>(AViking_Ram_Ship::StaticClass(), FVector(200.0f * i, 200.0f * i, 300.0f), FRotator(0.0f, 0.0f, 0.0f));
-		ShipVector.push_back(Ship);
-	}
+	////Generating ships
+	//for (int i = 0; i < 10; ++i) {
+	//	AViking_Ram_Ship* Ship = GetWorld()->SpawnActor<AViking_Ram_Ship>(AViking_Ram_Ship::StaticClass(), FVector(200.0f * i, 200.0f * i, 300.0f), FRotator(0.0f, 0.0f, 0.0f));
+	//	ShipVector.push_back(Ship);
+	//}
 
 }
 
 void AA_L_B_E_R_TGameModeBase::Tick(float DeltaSeconds) {
 	Super::Tick(DeltaSeconds);
 
-	for (const auto it : ShipVector) {
-		it->Target = GetWorld()->GetFirstPlayerController()->GetPawn()->GetActorLocation();
-	}
+	//for (const auto it : ShipVector) {
+	//	it->Target = GetWorld()->GetFirstPlayerController()->GetPawn()->GetActorLocation();
+	//}
 }
